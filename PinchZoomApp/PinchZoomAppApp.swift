0ea14a9fce5f2c1442740d009928fc7782bac54e
JiPolik <oleks.v.polishchuk@gmail.com>
2023-03-29 15:37:11 +0300

    Initial Commit

---
+++ PinchZoomApp/PinchZoomAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchZoomAppApp.swift
+//  PinchZoomApp
+//
+//  Created by  Oleksandr Polishchuk on 29.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchZoomAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
